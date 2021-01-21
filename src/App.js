f7e2de4d897384f6b8aec891dcac6c48e466d1b4
Noo <Noo@integrationworks.co.th>
2021-01-22 03:35:04 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Header } from 'semantic-ui-react'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header as='h2'>Refinitive pretest - Section2: Question2</Header>
+    </>
   );
 }
 
