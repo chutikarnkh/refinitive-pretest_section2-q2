87cc83a7ade276c9c97fe405db0445872aa702c1
Chutikarn Khumprom <noo@Chutikarns-Macbook-Pro.local>
2021-01-22 11:06:17 +0700

    Improve semantic table

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
-import {Input, Segment, Table } from 'semantic-ui-react'
+import './App.css'
+import { Input, Table } from 'semantic-ui-react'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   const [dataTable, setDataTable] = useState({ origin: [], temp: [] })
   const [searchInput, setSearchInput] = useState('')
-
-
 
   useEffect(() => {
     async function fetchCategories() {
@@ -22,29 +21,34 @@ function App() {
   }, [])
 
   useEffect(() => {
-    const filterData = dataTable.origin.filter((item)=> item.toLowerCase().indexOf(searchInput.toLowerCase()) >= 0)
-    setDataTable({...dataTable, temp: filterData})
+    const filterData = dataTable.origin.filter((item) => item.toLowerCase().indexOf(searchInput.toLowerCase()) >= 0)
+    setDataTable({ ...dataTable, temp: filterData })
   }, [searchInput])
 
   return (
-    <Segment className='container'>
-      <Input icon='search' placeholder='Search...' onChange={(e)=>setSearchInput(e.target.value)} />
-      <Table fixed>
+    <div className='container'>
+      <Input icon='search' placeholder='Search...' onChange={(e) => setSearchInput(e.target.value)} />
+      <Table basic>
         <Table.Header>
-          <Table.Row><Table.HeaderCell>Categories</Table.HeaderCell></Table.Row>
+          <Table.Row>
+            <Table.HeaderCell>#</Table.HeaderCell>
+            <Table.HeaderCell>Categories</Table.HeaderCell>
+          </Table.Row>
         </Table.Header>
         <Table.Body>
-          {dataTable.temp && dataTable.temp?.map((item, index) => {
-            return (
-              <Table.Row key={`item-${index}`}><Table.Cell>{item}</Table.Cell></Table.Row>
-            )
-          }
-          )}
+          {dataTable.temp &&
+            dataTable.temp?.map((item, index) => {
+              return (
+                <Table.Row key={`item-${index}`}>
+                  <Table.Cell>{index}</Table.Cell>
+                  <Table.Cell>{item}</Table.Cell>
+                </Table.Row>
+              )
+            })}
         </Table.Body>
       </Table>
-    </Segment>
-
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
